3ffb35cdf1e42afac49c95ea9542c0f9f5ed67ff
Edinson Requena <edinsonrequena@protonmail.com>
2023-08-27 09:39:48 -0400

    feat(adelanto de la pagina de experiencia)

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import Image from "next/image";
 
 export const Card = ({ image, description, peopleCount }) => {
+  const handleClick = () => {
+    console.log("Card clicked!");
+  };
+
   return (
-    <article className="relative rounded-lg shadow-md border border-gray-300">
-      <div className="relative">
-        <Image
-          src={image}
-          alt="Card"
-          layout="responsive"
-          width={340}
-          height={200}
-          className="rounded-t-lg"
-        />
-        <div className="absolute inset-0 bg-opacity-60 backdrop-blur-lg text-white p-4 flex flex-col justify-end">
-          <h2 className="text-lg font-semibold mb-1">{description}</h2>
-          <p className="text-sm">{peopleCount}</p>
+    <a href="#" className="block">
+      <article className="relative rounded-lg shadow-md border border-gray-300 mb-4">
+        <div className="relative">
+          <Image
+            src={image}
+            alt="Card"
+            layout="responsive"
+            width={340}
+            height={200}
+            className="rounded-t-lg"
+          />
+          <div className="absolute inset-0 bg-opacity-40 text-white backdrop-blur-sm p-4 flex flex-col justify-end">
+            <h2 className="text-lg font-semibold mb-1">{description}</h2>
+            <p className="text-sm">{peopleCount}</p>
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </a>
   );
 };
